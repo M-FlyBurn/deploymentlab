66099ebd365fb593f21b65b8c0c10a099d8628b5
m-flyburn <malika.clyburn@gmail.com>
2023-04-27 17:57:15 -0400

    changes-deploymentlab

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-app.listen(4000, () => console.log(`server running on 4000`))
 
+app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
-
+app.get('/',(req,res)=>{
+res.status(200).sendFile(path.join(__dirname,"../public/landing.html"))
+})
+app.listen(4000, () => console.log(`server running on 4000`))
